5c7843b88be89e735523cae974b426845ab88c8a
SMedintsev <sergmedin@gmail.com>
2015-07-21 22:28:21 +0300

    update

--- js/app/app.js
+++ js/app/app.js
@@ -5,39 +5,58 @@
     window.fileSystemGlobal = null;
     window.appName = window.appName || 'comix';
     window.serviceLink = "http://web421.newlinetechnologies.net/";
-   // window.serviceLink =  "http://localhost:50038/";
+    // window.serviceLink =  "http://localhost:50038/";
 
     angular
         .module(window.appName, ['ngRoute', 'ngAnimate', 'youtubePlayer'])  /*'youtubePlayer'*/
-        .config(['$routeProvider', '$locationProvider',  
+        .config(['$routeProvider', '$locationProvider',
             function ($routeProvider, $locationProvider) {
 
                 $routeProvider
                 .when('/loading', { templateUrl: 'js/partials/loading.html', controller: 'loadingCtrl' })
                 .when('/', { templateUrl: 'js/partials/home.html' })
-                .when('/title', { templateUrl: 'js/partials/title.html'})
+                .when('/title', { templateUrl: 'js/partials/title.html' })
                 .when('/about', { templateUrl: 'js/partials/about.html' })
                 .otherwise({ redirectTo: '/' });
-            
-                $locationProvider.html5Mode(false); 
-               
+
+                $locationProvider.html5Mode(false);
+
             }]);
-     
+
     angular
        .module(window.appName)
         .run(['$rootScope', '$http', '$location',
 
             function ($rootScope, $http, $location) {
-                    $location.url("/loading");
+                $location.url("/loading");
+
+                window.isphone = false;
+                if (document.URL.indexOf("http://") === -1
+                    && document.URL.indexOf("https://") === -1) {
+                    alert("isPhone");
+                    window.isphone = true;
+                }
+
+                if (window.isphone) {
+                    alert("isPhone 2");
+                    document.addEventListener("deviceready", onDeviceReady, false);
+                } else {
+                    alert("isPhone 3");
+                    onDeviceReady();
+                }
+
+                function onDeviceReady() {
+                    alert("isPhone 4");
+                }
             }
         ]);
-    
+
 
 }(window, window.angular));
 
 
 $(document).ready(function () {
-     
+
     $('.main-menu-btn').click(function () {
         $('#menuModal')
             .prop('class', 'modal fade') // revert to default
@@ -54,36 +73,36 @@ $(document).ready(function () {
     });
 
     $("#instagramm").click(function () {
-        var ref = window.open('https://instagram.com/', '_system', 'location=no');       
+        var ref = window.open('https://instagram.com/', '_system', 'location=no');
     });
 
     $("#twitter").click(function () {
-        var ref = window.open('https://twitter.com/sourceseek', '_system', 'location=no');    
+        var ref = window.open('https://twitter.com/sourceseek', '_system', 'location=no');
     });
 
     $("#tumbrl").click(function () {
-        var ref = window.open('https://www.tumblr.com/', '_system', 'location=no');      
+        var ref = window.open('https://www.tumblr.com/', '_system', 'location=no');
     });
 
-    
+
     function preventDefault(e) {
         e.preventDefault();
     };
-    
+
     $('#menuModal').on('shown.bs.modal', function (e) {
-  
+
         document.addEventListener('touchmove', preventDefault, false);
 
         $(document.body).addClass("remove-scroll");
-  
+
     });
 
     $('#menuModal').on('hidden.bs.modal', function (e) {
-    
+
         document.removeEventListener('touchmove', preventDefault, false);
 
-        $(document.body).removeClass("remove-scroll"); 
+        $(document.body).removeClass("remove-scroll");
     });
-    
+
 });
 
